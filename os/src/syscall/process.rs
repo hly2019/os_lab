b3d0c8f23b7609b7894a94a07514cdaf685d874b
hly <hly19@mails.tsinghua.edu.cn>
2022-04-06 17:19:22 +0800

    add sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,9 @@
 //! Process management syscalls
 
 use crate::config::MAX_SYSCALL_NUM;
-use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus};
+use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, current_user_token, TaskStatus};
 use crate::timer::get_time_us;
-
+use crate::mm::*;
 #[repr(C)]
 #[derive(Debug)]
 pub struct TimeVal {
@@ -33,12 +33,18 @@ pub fn sys_yield() -> isize {
 // YOUR JOB: 引入虚地址后重写 sys_get_time
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     let _us = get_time_us();
-    // unsafe {
-    //     *ts = TimeVal {
-    //         sec: us / 1_000_000,
-    //         usec: us % 1_000_000,
-    //     };
-    // }
+    let token = current_user_token();
+    let page_table = PageTable::from_token(token);
+    let start_va = VirtAddr::from(_ts as usize);
+    let vpn = start_va.floor();
+    let ppn = page_table.translate(vpn).unwrap().ppn().0;
+    let time_ptr = ppn << 12 | start_va.page_offset(); // ppn左移12位拼上offset
+    unsafe {
+        *(time_ptr as *mut TimeVal) = TimeVal {
+            sec: _us / 1_000_000,
+            usec: _us % 1_000_000,
+        };
+    }
     0
 }
 
@@ -58,5 +64,12 @@ pub fn sys_munmap(_start: usize, _len: usize) -> isize {
 
 // YOUR JOB: 引入虚地址后重写 sys_task_info
 pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    let token = current_user_token();
+    let page_table = PageTable::from_token(token);
+    let start_va = VirtAddr::from(ti as usize);
+    let vpn = start_va.floor();
+    let ppn = page_table.translate(vpn).unwrap().ppn().0;
+    let time_ptr = ppn << 12 | start_va.page_offset(); // ppn左移12位拼上offset
+
     -1
 }
